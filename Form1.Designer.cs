8257757d0af837ce3a61ea1ed94fdfe305bbb7dc
altan koroglu <altankoroglu@yahoo.com>
2021-03-14 21:54:25 +0300

    Proje yüklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace zilDers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gizleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gösterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Name = "label4";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gizleToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // gizleToolStripMenuItem
+            // 
+            this.gizleToolStripMenuItem.Name = "gizleToolStripMenuItem";
+            resources.ApplyResources(this.gizleToolStripMenuItem, "gizleToolStripMenuItem");
+            this.gizleToolStripMenuItem.Click += new System.EventHandler(this.gizleToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            resources.ApplyResources(this.çıkışToolStripMenuItem, "çıkışToolStripMenuItem");
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDown);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gösterToolStripMenuItem,
+            this.çıkışToolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            resources.ApplyResources(this.contextMenuStrip2, "contextMenuStrip2");
+            // 
+            // gösterToolStripMenuItem
+            // 
+            this.gösterToolStripMenuItem.Name = "gösterToolStripMenuItem";
+            resources.ApplyResources(this.gösterToolStripMenuItem, "gösterToolStripMenuItem");
+            this.gösterToolStripMenuItem.Click += new System.EventHandler(this.gösterToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem1
+            // 
+            this.çıkışToolStripMenuItem1.Name = "çıkışToolStripMenuItem1";
+            resources.ApplyResources(this.çıkışToolStripMenuItem1, "çıkışToolStripMenuItem1");
+            this.çıkışToolStripMenuItem1.Click += new System.EventHandler(this.çıkışToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.ForeColor = System.Drawing.Color.Gray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gizleToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem gösterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zilDers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        
+        DateTime basZaman = new DateTime();
+        DateTime bitZaman = new DateTime();
+        DateTime suAn = DateTime.Now;
+        string ntDakika, ntSaniye;
+        
+
+        TimeSpan kalanSure = new TimeSpan();
+
+        String[] zamanlar = { "08:20:00", "08:30:00", "09:00:00", "09:10:00", "09:40:00", "09:50:00", "10:20:00", "10:30:00", "11:00:00", "11:11:00", "11:40:00", "11:50:00", "12:20:00", "12:30:00", "13:00:00", "13:10:00", "13:40:00", "15:50:00", "16:20:00", "16:30:00", "17:00:00", "17:10:00", "17:40:00", "17:50:00", "18:20:00", "18:30:00", "19:00:00", "19:10:00", "19:40:00", "19:50:00", "20:20:00"};
+        //dizi içinde indeksi çift numarada olanlar teneffüs; tek numarada olanlar ise dersi ifade ediyor. 
+        string bugun,mesaj;
+
+        int pan,i=0;
+
+        protected void panKucuk()
+        {
+            pan = 0;
+            this.Width = 50;
+            label4.Left = 10;
+            label6.Visible = false;
+            this.Left = Screen.PrimaryScreen.WorkingArea.Size.Width - this.Width;
+            this.Top= Screen.PrimaryScreen.WorkingArea.Size.Height - this.Height;
+
+        }
+
+        protected void panBuyuk()
+        {
+            pan = 1;
+            this.Width = 420;
+            label4.Left = 330;
+            label6.Visible = true;
+            this.Left = Screen.PrimaryScreen.WorkingArea.Size.Width - this.Width;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Size.Height - this.Height;
+        }
+
+
+        protected void aralikBul()
+        {
+                for (i = 0; i <= zamanlar.Length; i++)
+                {
+                    basZaman = Convert.ToDateTime(bugun + " " + zamanlar[i]);
+                    bitZaman = Convert.ToDateTime(bugun + " " + zamanlar[i + 1]);
+
+                    if (suAn >= basZaman && suAn <= bitZaman)
+                    {
+                    if (i % 2 == 0)
+                    {
+                        mesaj = " Başlamasına";
+                        this.BackColor= Color.LightPink;
+                    }
+                   else
+                    {
+                        mesaj = " Bitimine";
+                        this.BackColor = Color.LemonChiffon;
+                    }
+                        
+                        label6.Text = (i/2+1)+". Dersin" + mesaj + " Kalan Süre:";
+                        break;
+                    }
+
+                }
+
+  }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            bugun = DateTime.Now.ToShortDateString();
+            aralikBul();            
+            panKucuk();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            suAn = DateTime.Now;
+            kalanSure = bitZaman - suAn;
+
+            ntDakika = string.Format(@"{0:%m}", kalanSure);
+            ntSaniye= string.Format(@"{0:%s}", kalanSure);
+
+
+            if (pan == 0)
+            {
+                label4.Text = string.Format(@"{0:%m}", kalanSure);
+                if (label4.Text == "0") label4.Text = string.Format(@"{0:%s}", kalanSure);
+                if (label4.Text == "0") aralikBul();
+                
+            }
+
+
+            if (pan == 1) label4.Text = string.Format(@"{0:mm\:ss}", kalanSure);
+            if (label4.Text == "00:00") aralikBul();
+
+        }
+                
+  
+
+   
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Opacity = .70;
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            label6.Enabled = false;
+        }
+
+  
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                //pan yani panel değişkeni kutunun küçük durumda mı büyük durumda mı olduğunu gösteriyor.
+                //pan=0 --> küçük durumda
+                //pan=1 --> büyük durumda
+                if (pan == 0) panBuyuk();
+                else if (pan == 1) panKucuk();
+            }
+            
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void gizleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void gösterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void çıkışToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+       
+
+        private void notifyIcon1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)                
+            {
+
+                //notifyIcon1.Visible = true;               
+                // notifyIcon1.ShowBalloonTip(2000, label6.Text, ntDakika+" Dakika "+ntSaniye+" Saniye", ToolTipIcon.Info);
+
+                notifyIcon1.Icon = SystemIcons.Exclamation;
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(5000, "Welcome", "Hello ", ToolTipIcon.Info);
+
+            }
+        }
+
+       
+            
+   
+
+    }
+}
